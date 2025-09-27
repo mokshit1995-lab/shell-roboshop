93f731f88d583ba6788e9266a844f4e6810ceeb7
Gunti <mokshit.gunti@dxc.com>
2025-09-27 20:45:21 +0530

    Commited

--- robo-instance-crt.sh
+++ robo-instance-crt.sh
@@ -19,7 +19,7 @@ do
     fi
 
     echo "$instance: $IP"
-    INSTANCE_NAME=$(instance)
+    INSTANCE_NAME=$($instance)
 
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
