674f0b6b00d7d4811c86a838000cd0096a6c5aec
Gunti <mokshit.gunti@dxc.com>
2025-10-01 16:23:21 +0530

    Commit

--- catalogue.sh
+++ catalogue.sh
@@ -81,7 +81,7 @@ VALIDATE $? "Adding Mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB client"
 
-INDEX=$(mongosh mongodb.mgunti.space --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
--- mongodb.sh
+++ mongodb.sh
@@ -30,19 +30,19 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
 
-dnf install mongodb-org -y  &>>LOG_FILE
+dnf install mongodb-org -y  &>>$LOG_FILE
 VALIDATE $? "Installing mongodb"
 
 
-systemctl enable mongod &>>LOG_FILE
+systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enabling mongodb"
 
-systemctl start mongod &>>LOG_FILE
+systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Starting mongodb"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing repmote connections to mongodb"
 
-systemctl restart mongod &>>LOG_FILE
+systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "Restarting mongodb"
 
